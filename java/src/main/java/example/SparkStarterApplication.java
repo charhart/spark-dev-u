ad5cca302baa4e735893e78d6f0904530e9c1da5
Charity Hilton <cahilton@regenstrief.org>
2016-05-30 16:45:03 -0400

    add starter java

---
+++ java/src/main/java/example/SparkStarterApplication.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkStarterApplication.class, args);
+	}
+}
